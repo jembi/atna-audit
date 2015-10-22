4a11063df835dcef46fcaa9de6de52a23f85c036
Ryan Crichton <rg.crichton@gmail.com>
2015-10-22 16:50:10 +0200

    Update package.json

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const atna = require('./index');
 
 let audit = atna.userLoginAudit(0, 'openhim', 'x.x.x.x', 'testUser', 'testRole', '123');
 atna.validateAudit(audit);
+console.log(audit);
 
 let syslog = atna.wrapInSyslog('test');
 console.log(syslog);
